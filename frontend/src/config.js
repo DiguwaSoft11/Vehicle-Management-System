2c6b293ea70422f1eb629b4a2ab313e4eaa800cb
DiguwaSoft <diguwasoft@gmail.com>
2025-07-07 11:55:04 +0300

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 
-export const BASE_URL = "https://vms-backend.up.railway.app/api/v1";
+export const BASE_URL = "https://vm-backend.up.railway.app/api/v1";
 export const token = localStorage.getItem("token");
